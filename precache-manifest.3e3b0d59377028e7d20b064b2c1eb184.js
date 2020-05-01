4270c94f19f8ba83ad2348f77100ace223a76c31
RuoyuWang <klaus1201810802@gmail.com>
2020-05-01 02:39:20 +0100

    Updates

---
+++ precache-manifest.3e3b0d59377028e7d20b064b2c1eb184.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c949c3c61fdc4b6b8f00069df133760",
+    "url": "/beijing-airbnb-vis/index.html"
+  },
+  {
+    "revision": "2225b455882d77a3d4fd",
+    "url": "/beijing-airbnb-vis/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "1c0dfa11f1a39a68e8c4",
+    "url": "/beijing-airbnb-vis/static/css/main.79308cf0.chunk.css"
+  },
+  {
+    "revision": "2225b455882d77a3d4fd",
+    "url": "/beijing-airbnb-vis/static/js/2.666a9d83.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/beijing-airbnb-vis/static/js/2.666a9d83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c0dfa11f1a39a68e8c4",
+    "url": "/beijing-airbnb-vis/static/js/main.e13bcdf5.chunk.js"
+  },
+  {
+    "revision": "f764b4b6f094416ba651",
+    "url": "/beijing-airbnb-vis/static/js/runtime-main.91f82d2c.js"
+  }
+]);
